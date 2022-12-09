c11ecdf713a228cb6c9a76506094c441f6f4af3a
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-09 15:16:52 -0300

    login 0.0.5

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,18 @@ import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function Header() {
+
+    const dispatch = useDispatch();
+
+    const logoutOfApp = () => {
+        dispatch(logout())
+        auth.signOut();
+    }
     return (
         <div className='header'>
             <div className="header__left">
@@ -27,7 +37,10 @@ function Header() {
                 <HeaderOption Icon={ChatIcon} title="Messaging"/>
                 <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
                 <HeaderOption 
-                avatar="https://avatars.githubusercontent.com/u/101577825?v=4" title='Me' />
+                avatar="https://avatars.githubusercontent.com/u/101577825?v=4" 
+                title='Me' 
+                onClick={logoutOfApp}
+                />
             </div>
         </div>
     )
