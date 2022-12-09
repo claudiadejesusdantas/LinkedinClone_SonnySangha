f675e5b14db019541faa27281ad041099e3c318c
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-12-09 13:27:22 -0300

    login 0.0.4

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css';
 
 function Login(){
-    const loginToApp = {} = {};
-    
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const[name, setName] = useState('');
+    const[profilePic, setProfilePic] = useState('');
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+    }
+
     const register = {} = {};
 
     return(
@@ -11,16 +18,17 @@ function Login(){
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt="Logo do linkedin"/>
 
             <form>
-                <input placeholder="Full name(required if registering)" type='text'/>
-                <input placeholder="Profile pic URL (optional)" type='text'/>
-                <input placeholder="Email" type='email'/>
-                <input placeholder="Password" type='password'/>
+                <input value={name} onChange={e=> setName(e.target.value)}placeholder="Full name(required if registering)" type='text'/>
+                <input value={profilePic} onChange={e=> setProfilePic(e.target.value)} placeholder="Profile pic URL (optional)" type='text'/>
+                <input value={email} onChange={e=> setEmail(e.target.value)} placeholder="Email" type='email'/>
+                <input value={password} onChange={e=> setPassword(e.target.value)} placeholder="Password" type='password'/>
 
                 <button type='submit' onClick={loginToApp}>Sing in</button>
             </form>
 
-            <p>Not a member?</p>
-            <span className='login__register'>Register Now</span>
+            <p>Not a member? {" "}
+                <span className='login__register'> Register Now</span>
+            </p>
         </div>
     )
 }
